0ce1a6a81a0450b0027dcf3cf6b958a561676675
satnamsidhu425@gmail.com <satnamsidhu425@gmail.com>
2018-06-06 11:07:49 -0700

    created intern class

---
+++ src/com/lambton/term/Intern.java
@@ -0,0 +1,14 @@
+package com.lambton.term;
+
+public class Intern extends Employee{
+	private String schoolName;
+
+	public String getSchoolName() {
+		return schoolName;
+	}
+	
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	
+}
